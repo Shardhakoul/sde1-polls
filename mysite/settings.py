a7758f170c0b8081515e8b4bb995836d85d3388c
Divya Gupta <divyagupta0928@gmail.com>
2021-10-15 19:36:38 -0400

    environment variable check

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jc_r!b!=+*!di&c0eocdy9-ud^5x9+a%q#x6%a)rdij@y%(%!c'
+#SECRET_KEY = 'django-insecure-jc_r!b!=+*!di&c0eocdy9-ud^5x9+a%q#x6%a)rdij@y%(%!c'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
